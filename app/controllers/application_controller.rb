59bc09e0898090b185f4eb3b728a76e8106ef951
Sagid, home <kofon95@mail.ru>
2018-02-28 22:14:49 +0300

    Add devise authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  private
+    def configure_permitted_parameters
+      keys = [:first_name, :last_name, :patronymic, :birthday]
+      devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+      devise_parameter_sanitizer.permit(:account_update, keys: keys)
+    end
+
+    def after_sign_up_path_for(resource)
+      '/'
+    end
+
+    # def after_sign_in_path_for(resource)
+    #   '/'
+    # end
 end
