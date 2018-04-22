d15e5ab5bfd550a72cdb37f659de7d92adcd6860
yusupovskiy <yusupovskiy.rasul@mail.ru>
2018-04-22 15:52:18 +0300

    Изменил шаблон по умолчанию

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :ensure_current_user
   before_action :authenticate_user!
+  layout 'card'
 
 
   private
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  layout 'application'
   def index
     # NotificationMailer.welcome.deliver_later
   end
