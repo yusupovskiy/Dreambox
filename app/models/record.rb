207ca17684733e757bf7e4e05b6dc661eb0d8e27
yusupovskiy <yusupovskiy.rasul@mail.ru>
2018-05-27 17:11:56 +0300

    Подправил структур

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,9 +4,12 @@ class Record < ApplicationRecord
   has_many :record_service
   has_many :services, through: :record_service
 
-  enum record_type: {grouping: 0, personal: 1}
-  enum visit_type: {one_time_visit: 0, reusable_visit: 1,
-                    one_time_abon: 2, automatic_abon: 3, dynamic_abon: 4}
+  # enum record_type: {grouping: 0, personal: 1}
+  # enum visit_type: {one_time_visit: 0, reusable_visit: 1,
+  #                   one_time_abon: 2, automatic_abon: 3, dynamic_abon: 4}
+
+  enum record_type: {grouping: 0}
+  enum visit_type: {one_time_abon: 0}
 
   validates :name, :record_type, :visit_type, :created_at, :finished_at, :abon_period, :affiliate_id, presence: true
 end
